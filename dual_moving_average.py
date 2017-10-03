c4db4067de007781db076c7c8e5e5f897cc55945
Bryan Hansen <bryguy1300@gmail.com>
2017-10-02 23:45:41 -0700

    Now loading VIX

--- dual_moving_average.py
+++ dual_moving_average.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 import pytz
 
-from zipline.api import order, record, symbol
+from zipline.api import order, record, symbol, fetch_csv
 from zipline import TradingAlgorithm
 
+QUANDL_API_KEY = "vatGLfsDnNH6tAwvwzV6"
 
 def initialize(context):
+    #                 Date  VIX Open  VIX High  VIX Low  VIX Close
+    # 0     2017-10-02      9.59     10.04     9.37       9.45
+    fetch_csv('https://www.quandl.com/api/v3/datasets/CBOE/VIX.csv?api_key=' + QUANDL_API_KEY, symbol='VIX', date_column='Date')
     pass
 
-
 def handle_data(context, data):
+    order(symbol('VXX'), 15)
+    record(VXX=data[symbol('VXX')].price)
+
     order(symbol('XIV'), 15)
     record(XIV=data[symbol('XIV')].price)
+
+    print data.current('VIX', 'VIX Close')
--- evaluate_results.py
+++ evaluate_results.py
@@ -11,15 +11,15 @@ RESULTS_DIR = "results/"
 RESULTS_PLOTS_DIR = RESULTS_DIR + "plots/"
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('source_file', help='Pickle file to evaluate')
-	parser.add_argument( "-p", "--plot", help="plot graphs", action="store_true")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_file', help='Pickle file to evaluate')
+    parser.add_argument( "-p", "--plot", help="plot graphs", action="store_true")
+    args = parser.parse_args()
 
-	if not os.path.isfile(args.source_file):
-		sys.exit("Invalid source file: " + args.source_file)
+    if not os.path.isfile(args.source_file):
+        sys.exit("Invalid source file: " + args.source_file)
 
-	locale.setlocale( locale.LC_ALL, '' )
+    locale.setlocale( locale.LC_ALL, '' )
 
 # Zipline columns:
 # Index([u'AAPL','algo_volatility','algorithm_period_return','alpha',
@@ -33,43 +33,43 @@ def main():
 #        u'starting_cash','starting_exposure','starting_value',
 #        u'trading_days','transactions','treasury_period_return'],
 
-	dataFrame = pd.read_pickle(args.source_file)
+    dataFrame = pd.read_pickle(args.source_file)
 
-	printSummary(dataFrame)
+    printSummary(dataFrame)
 
-	if args.plot:
-   		plotData(dataFrame)
+    if args.plot:
+        plotData(dataFrame)
 
 def plotData(dataFrame):
-	if not os.path.exists(RESULTS_PLOTS_DIR):
-		os.makedirs(RESULTS_PLOTS_DIR)
+    if not os.path.exists(RESULTS_PLOTS_DIR):
+        os.makedirs(RESULTS_PLOTS_DIR)
 
-	plots = [("Position Balance", dataFrame.ending_value), \
-			 ("Returns", dataFrame.returns), \
-			 ("PNL", dataFrame.pnl), \
-			 ("Alpha", dataFrame.alpha), \
-			 ("Total Balance", dataFrame.portfolio_value), \
-			 ("Cash Balance", dataFrame.ending_cash)]
-	for index, plotItem in enumerate(plots):
-		plt.figure(index)
-		plotItem[1].plot(title=plotItem[0], secondary_y=True, grid=True, figsize=(12,6), alpha=0.75, color="blue")
-		plt.savefig(RESULTS_PLOTS_DIR + 'plot%d.png' % index)
+    plots = [("Position Balance", dataFrame.ending_value), \
+             ("Returns", dataFrame.returns), \
+             ("PNL", dataFrame.pnl), \
+             ("Alpha", dataFrame.alpha), \
+             ("Total Balance", dataFrame.portfolio_value), \
+             ("Cash Balance", dataFrame.ending_cash)]
+    for index, plotItem in enumerate(plots):
+        plt.figure(index)
+        plotItem[1].plot(title=plotItem[0], secondary_y=True, grid=True, figsize=(12,6), alpha=0.75, color="blue")
+        plt.savefig(RESULTS_PLOTS_DIR + 'plot%d.png' % index)
 
 def printSummary(dataFrame):
-	startingPortfolioValue = dataFrame.portfolio_value[0]
-	totalProfit = dataFrame.pnl.sum()
-	totalCapitalOutlay = -(dataFrame.capital_used.sum())
-	totalReturn = totalProfit / totalCapitalOutlay
+    startingPortfolioValue = dataFrame.portfolio_value[0]
+    totalProfit = dataFrame.pnl.sum()
+    totalCapitalOutlay = -(dataFrame.capital_used.sum())
+    totalReturn = totalProfit / totalCapitalOutlay
 
-	print "=========== Portfolio Summary ==========="
-	print " Starting Value: %s" % locale.currency(startingPortfolioValue, grouping=True)
-	print "                   "
-	print " Captial Outlay: %s" % (locale.currency(totalCapitalOutlay, grouping=True))
-	print "    Profit/Loss: %s (%s)" % (locale.currency(totalProfit, grouping=True), '{:.2%}'.format(totalReturn))
-	print " Position Value: %s" % locale.currency(dataFrame.ending_value[-1], grouping=True)
-	print "                   "
-	print "Portfolio Total: %s" % locale.currency(dataFrame.portfolio_value[-1], grouping=True)
-	print "========================================="
+    print "=========== Portfolio Summary ==========="
+    print " Starting Value: %s" % locale.currency(startingPortfolioValue, grouping=True)
+    print "                   "
+    print " Captial Outlay: %s" % (locale.currency(totalCapitalOutlay, grouping=True))
+    print "    Profit/Loss: %s (%s)" % (locale.currency(totalProfit, grouping=True), '{:.2%}'.format(totalReturn))
+    print " Position Value: %s" % locale.currency(dataFrame.ending_value[-1], grouping=True)
+    print "                   "
+    print "Portfolio Total: %s" % locale.currency(dataFrame.portfolio_value[-1], grouping=True)
+    print "========================================="
 
 if __name__ == "__main__":
     main()
